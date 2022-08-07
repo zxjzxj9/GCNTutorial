2e7e32a6f407815c17c2b9909403882894374674
Victor Zhang <zxjzxj9@gmail.com>
2022-08-07 22:28:11 +0800

    add init method for lgihtgcn

--- ex01_lightgcn/model.py
+++ ex01_lightgcn/model.py
@@ -1,4 +1,13 @@
 #! /usr/bin/env python
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import dgl
+
+class LightGCN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
