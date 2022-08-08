6f285955c3b07e4e1bd6f78942d26b486e61b606
Victor Zhang <zxjzxj9@gmail.com>
2022-08-08 23:49:59 +0800

    add gcn module

--- ex01_lightgcn/model.py
+++ ex01_lightgcn/model.py
@@ -4,10 +4,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import dgl
+from dgl.nn.pytorch import GraphConv
 
 class LightGCN(nn.Module):
     def __init__(self):
         super().__init__()
+        self.conv1 = GraphConv(32, 32, norm='both', weight=True, bias=True)
 
     def forward(self, x):
         pass
