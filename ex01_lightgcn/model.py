ab3a1cc8aeedc12d3b048475a9323a8c6c47efcf
Victor Zhang <zxjzxj9@gmail.com>
2022-08-11 22:47:13 +0800

    add light gcn embeddings

--- ex01_lightgcn/model.py
+++ ex01_lightgcn/model.py
@@ -4,11 +4,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import dgl
-from dgl.nn.pytorch import GraphConv
+from dgl.nn.pytorch import GraphConv, NodeEmbedding
 
 class LightGCN(nn.Module):
-    def __init__(self):
+    """ LightGCN module
+
+    """
+    def __init__(self, num_nodes, hiddens):
         super().__init__()
+        self.embed = NodeEmbedding(num_nodes, hiddens[0])
         self.conv1 = GraphConv(32, 32, norm='both', weight=True, bias=True)
         self.conv2 = GraphConv(32, 32, norm='both', weight=True, bias=True)
 
