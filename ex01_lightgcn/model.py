ff7ebd53129f08dfde836e9d882b93410da928ee
Victor Zhang <zxjzxj9@gmail.com>
2022-08-23 23:41:42 +0800

    fix init function issue

--- ex01_lightgcn/model.py
+++ ex01_lightgcn/model.py
@@ -6,19 +6,23 @@ import torch.nn.functional as F
 import dgl
 from dgl.nn.pytorch import GraphConv, NodeEmbedding
 
+def init_func(embed):
+    nn.init.uniform_(embed, -1e-3, 1e-3)
+    return embed
+
 class SimpleGCN(nn.Module):
     """ LightGCN module
 
     """
     def __init__(self, num_nodes, hiddens):
         super().__init__()
-        self.embed = NodeEmbedding(num_nodes, hiddens[0], 'node_emb')
+        self.embed = NodeEmbedding(num_nodes, hiddens[0], 'node_emb', init_func=init_func)
         self.conv1 = GraphConv(32, 32, norm='both', weight=True, bias=True)
         self.conv2 = GraphConv(32, 32, norm='both', weight=True, bias=True)
         self.conv3 = GraphConv(32, 1, norm='both', weight=True, bias=True)
 
     def forward(self, g:dgl.DGLGraph):
-        x = self.embed(g.nodes)
+        x = self.embed(g.nodes())
         x = F.relu(self.conv1(g, x))
         x = F.relu(self.conv2(g, x))
         x = self.conv3(x).squeeze(-1)
