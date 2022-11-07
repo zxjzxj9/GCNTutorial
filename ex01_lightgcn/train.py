435d55e976b8d8fe217598a1bb41c92caeb83ba2
Victor Zhang <zxjzxj9@gmail.com>
2022-11-07 23:14:31 +0800

    add print, fix the cache issue

--- ex01_lightgcn/train.py
+++ ex01_lightgcn/train.py
@@ -21,6 +21,7 @@ opts.add_argument('-m', '--num-layers', nargs='+', help='number of gcn layers', 
 def train(args):
     dataset = GowallaCheckIns()
     graph: dgl.DGLGraph = dataset.graph
+    print(graph)
     graph = graph #.to('cuda:0')
     sampler = dgl.sampling.PinSAGESampler(graph, "user", "item", 3, 0.5, 200, 10)
     seeds = torch.LongTensor([0, 1, 2])
